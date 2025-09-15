05a3be12698ad25c3f38a3c4987c25dd96f7de1f
Madhusudan <madhusudandande92@gmail.com>
2025-09-15 23:28:54 +0530

    PUMA added successfully

---
+++ PUMA/src/main/java/com/shop/model/Student.java
@@ -0,0 +1,48 @@
+/**
+ * 
+ */
+package com.shop.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+/**
+ * 
+ */
+@Entity
+@Table(name="Student_Data")
+public class Student {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private long id;
+	private String name;
+	private String city;
+	public long getId() {
+		return id;
+	}
+	public void setId(long id) {
+		this.id = id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getCity() {
+		return city;
+	}
+	public void setCity(String city) {
+		this.city = city;
+	}
+	@Override
+	public String toString() {
+		return "Student [id=" + id + ", name=" + name + ", city=" + city + "]";
+	}
+	
+	
+}
---
+++ PUMA/src/main/java/com/shop/repo/StudentRepo.java
@@ -0,0 +1,15 @@
+/**
+ * 
+ */
+package com.shop.repo;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.shop.model.Student;
+
+/**
+ * 
+ */
+public interface StudentRepo extends JpaRepository<Student, Long>{
+
+}
